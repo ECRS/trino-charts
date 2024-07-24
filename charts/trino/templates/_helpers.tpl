cf9df6b281047a565398caef22bc4b6e2929c7b6
Joe Jensen <jjensen@ecrs.com>
2024-07-24 14:58:50 -0400

    Synchronize with upstream

--- charts/trino/templates/_helpers.tpl
+++ charts/trino/templates/_helpers.tpl
@@ -16,8 +16,8 @@ If release name contains chart name it will be used as a full name.
 {{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
 {{- else }}
 {{- $name := default .Chart.Name .Values.nameOverride }}
-{{- if contains $name .Release.Name }}
-{{- .Release.Name | trunc 63 | trimSuffix "-" }}
+{{- if hasPrefix .Release.Name $name }}
+{{- $name | trunc 63 | trimSuffix "-" }}
 {{- else }}
 {{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" }}
 {{- end }}
@@ -36,10 +36,10 @@ Create chart name and version as used by the chart label.
 {{- .Values.coordinatorNameOverride | trunc 63 | trimSuffix "-" }}
 {{- else }}
 {{- $name := default .Chart.Name .Values.nameOverride }}
-{{- if contains $name .Release.Name }}
-{{- .Release.Name | trunc 63 | trimSuffix "-" }}-coordinator
+{{- if hasPrefix .Release.Name $name }}
+{{- printf "%s-%s" $name "coordinator" | trunc 63 | trimSuffix "-" }}
 {{- else }}
-{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" }}-coordinator
+{{- printf "%s-%s-%s" .Release.Name $name "coordinator" | trunc 63 | trimSuffix "-" }}
 {{- end }}
 {{- end }}
 {{- end }}
@@ -49,10 +49,10 @@ Create chart name and version as used by the chart label.
 {{- .Values.workerNameOverride | trunc 63 | trimSuffix "-" }}
 {{- else }}
 {{- $name := default .Chart.Name .Values.nameOverride }}
-{{- if contains $name .Release.Name }}
-{{- .Release.Name | trunc 63 | trimSuffix "-" }}-worker
+{{- if hasPrefix .Release.Name $name }}
+{{- printf "%s-%s" $name "worker" | trunc 63 | trimSuffix "-" }}
 {{- else }}
-{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" }}-worker
+{{- printf "%s-%s-%s" .Release.Name $name "worker" | trunc 63 | trimSuffix "-" }}
 {{- end }}
 {{- end }}
 {{- end }}
@@ -72,6 +72,9 @@ helm.sh/chart: {{ include "trino.chart" . }}
 app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
 {{- end }}
 app.kubernetes.io/managed-by: {{ .Release.Service }}
+{{- if .Values.commonLabels }}
+{{ tpl (toYaml .Values.commonLabels) . }}
+{{- end }}
 {{- end }}
 
 {{/*
@@ -90,5 +93,49 @@ Create the name of the service account to use
 {{- default (include "trino.fullname" .) .Values.serviceAccount.name }}
 {{- else }}
 {{- default "default" .Values.serviceAccount.name }}
+{{- end }}
+{{- end }}
+
+{{/*
+Return the proper image name
+{{ include "trino.image" . }}
+
+Code is inspired from bitnami/common
+
+*/}}
+{{- define "trino.image" -}}
+{{- $repositoryName := .Values.image.repository -}}
+{{- if .Values.image.useRepositoryAsSoleImageReference -}}
+  {{- printf "%s" $repositoryName -}}
+{{- else -}}
+  {{- $repositoryName := .Values.image.repository -}}
+  {{- $registryName := .Values.image.registry -}}
+  {{- $separator := ":" -}}
+  {{- $termination := (default .Chart.AppVersion .Values.image.tag) | toString -}}
+  {{- if .Values.image.digest }}
+    {{- $separator = "@" -}}
+    {{- $termination = .Values.image.digest | toString -}}
+  {{- end -}}
+  {{- if $registryName }}
+    {{- printf "%s/%s%s%s" $registryName $repositoryName $separator $termination -}}
+  {{- else -}}
+    {{- printf "%s%s%s"  $repositoryName $separator $termination -}}
+  {{- end -}}
+{{- end -}}
+{{- end -}}
+
+{{/*
+Create the name of the file auth secret to use
+*/}}
+{{- define "trino.fileAuthSecretName" -}}
+{{- if and .Values.auth .Values.auth.passwordAuthSecret }}
+{{- .Values.auth.passwordAuthSecret | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- $name := default .Chart.Name .Values.nameOverride }}
+{{- if hasPrefix .Release.Name $name }}
+{{- printf "%s-%s" $name "file-authentication" | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- printf "%s-%s-%s" .Release.Name $name "file-authentication" | trunc 63 | trimSuffix "-" }}
+{{- end }}
 {{- end }}
 {{- end }}
---
+++ test.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+declare -A testCases=(
+    [default]=""
+    [single_node]="--set server.workers=0"
+    [complete_values]="--values test-values.yaml"
+    [overrides]="--set coordinatorNameOverride=coordinator-overridden,workerNameOverride=worker-overridden,nameOverride=overridden"
+)
+
+function join_by {
+    local d=${1-} f=${2-}
+    if shift 2; then
+        printf %s "$f" "${@/#/$d}"
+    fi
+}
+
+# default to randomly generated namespace, same as chart-testing would do, but we need to load secrets into the same namespace
+NAMESPACE=trino-$(LC_ALL=C tr -dc 'a-z0-9' </dev/urandom | head -c 6 || true)
+HELM_EXTRA_SET_ARGS=
+CT_ARGS=(--charts=charts/trino --skip-clean-up --helm-extra-args="--timeout 2m")
+CLEANUP_NAMESPACE=true
+TEST_NAMES=(default single_node complete_values)
+
+usage() {
+    cat <<EOF 1>&2
+Usage: $0 [-h] [-n <NAMESPACE>] [-a <HELM_EXTRA_SET_ARGS>] [-t <TESTS>] [-s]
+Test the Trino chart
+
+-h       Display help
+-n       Kubernetes namespace, a randomly generated one is used if not provided
+-a       Extra Helm set args
+-t       Test names to run, comma separated; defaults to $(join_by , "${TEST_NAMES[@]}")
+-s       Skip chart cleanup
+EOF
+}
+
+while getopts ":a:n:t:sh:" OPTKEY; do
+    case "${OPTKEY}" in
+        a)
+            HELM_EXTRA_SET_ARGS=${OPTARG}
+            ;;
+        n)
+            NAMESPACE=${OPTARG}
+            ;;
+        t)
+            IFS=, read -ra TEST_NAMES <<<"$OPTARG"
+            ;;
+        s)
+            CLEANUP_NAMESPACE=false
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+cd "${SCRIPT_DIR}" || exit 2
+
+echo 1>&2 "Generating a self-signed TLS certificate"
+openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
+    -subj "/O=Trino Software Foundation" \
+    -addext "subjectAltName=DNS:localhost,DNS:*.$NAMESPACE,DNS:*.$NAMESPACE.svc,DNS:*.$NAMESPACE.svc.cluster.local,IP:127.0.0.1" \
+    -keyout cert.key -out cert.crt
+
+kubectl create namespace "$NAMESPACE" --dry-run=client --output yaml | kubectl apply --filename -
+kubectl -n "$NAMESPACE" create secret tls certificates --cert=cert.crt --key=cert.key --dry-run=client --output yaml | kubectl apply --filename -
+
+# only install the Prometheus Helm chart when running the `complete_values` test
+if printf '%s\0' "${TEST_NAMES[@]}" | grep -qwz complete_values; then
+    helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+    helm upgrade --install prometheus-operator prometheus-community/kube-prometheus-stack -n "$NAMESPACE" \
+        --version "60.0.2" \
+        --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false \
+        --set grafana.enabled=false
+    kubectl rollout status --watch deployments -l release=prometheus-operator -n "$NAMESPACE"
+fi
+
+CT_ARGS+=(--namespace "$NAMESPACE")
+
+result=0
+for test_name in "${TEST_NAMES[@]}"; do
+    echo 1>&2 ""
+    echo 1>&2 "🧪 Running test $test_name"
+    echo 1>&2 ""
+    if ! time ct install "${CT_ARGS[@]}" --helm-extra-set-args "$HELM_EXTRA_SET_ARGS ${testCases[$test_name]}"; then
+        echo 1>&2 "❌ Test $test_name failed"
+        echo 1>&2 "Test logs:"
+        kubectl --namespace "$NAMESPACE" logs --tail=-1 --selector app.kubernetes.io/component=test --all-containers=true
+        result=1
+    else
+        echo 1>&2 "✅ Test $test_name completed"
+    fi
+    if [ "$CLEANUP_NAMESPACE" == "true" ]; then
+        for release in $(helm --namespace "$NAMESPACE" ls --all --short | grep -v 'prometheus-operator'); do
+            echo 1>&2 "Cleaning up Helm release $release"
+            helm --namespace "$NAMESPACE" delete "$release"
+        done
+    fi
+done
+
+if [ "$CLEANUP_NAMESPACE" == "true" ]; then
+    helm -n "$NAMESPACE" uninstall prometheus-operator --ignore-not-found
+    kubectl delete namespace "$NAMESPACE"
+    mapfile -t crds < <(kubectl api-resources --api-group=monitoring.coreos.com --output name)
+    if [ ${#crds[@]} -ne 0 ]; then
+        kubectl delete crd "${crds[@]}"
+    fi
+fi
+
+exit $result
